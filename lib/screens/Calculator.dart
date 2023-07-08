025e23d0d719573431a381e945b38f8a10847f96
Ahbaly <123125056+Ahbaly@users.noreply.github.com>
2023-07-08 22:54:17 +0100

    task5 -> radio click (already done) task6 -> handle exceptions  / show dialog

--- lib/screens/Calculator.dart
+++ lib/screens/Calculator.dart
@@ -6,12 +6,13 @@ class Calculator extends StatefulWidget {
 
   @override
   State<Calculator> createState() => _CalculatorState();
-
 }
 
 class _CalculatorState extends State<Calculator> {
-  late String? _op = "+";
+  final TextEditingController _number1Controller = TextEditingController();
+  final TextEditingController _number2Controller = TextEditingController();
 
+  late String? _op = "+";
 
   @override
   Widget build(BuildContext context) {
@@ -29,54 +30,81 @@ class _CalculatorState extends State<Calculator> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextField(
+            controller: _number1Controller,
             decoration: InputDecoration(hintText: "First number"),
           ),
-
           RadioListTile(
             title: Text("+"),
             value: "+",
             groupValue: _op,
-            onChanged: (value){
+            onChanged: (value) {
               setState(() {
                 _op = value.toString();
-                print(_op);
               });
             },
           ),
-
           RadioListTile(
             title: Text("-"),
             value: "-",
             groupValue: _op,
-            onChanged: (value){
+            onChanged: (value) {
               setState(() {
                 _op = value.toString();
-                print(_op);
-
               });
             },
           ),
-
           RadioListTile(
             title: Text("*"),
             value: "*",
             groupValue: _op,
-            onChanged: (value){
+            onChanged: (value) {
               setState(() {
                 _op = value.toString();
-                print(_op);
-
               });
             },
           ),
-
           TextField(
+            controller: _number2Controller,
             decoration: InputDecoration(hintText: "Second number"),
-
           ),
-
           ElevatedButton(
             onPressed: () {
+              try {
+                int number1 = int.parse(_number1Controller.text);
+                int number2 = int.parse(_number2Controller.text);
+
+                print("good");
+
+                // Navigator.of(context).push(MaterialPageRoute(
+                //     builder: (context) =>
+                //         mainContainer(CalculationScreen(number, _operation))));
+              } on Exception {
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Caution"),
+                      content: Row(
+                        children: [
+                          Icon(
+                            Icons.info,
+                            color: Colors.black,
+                          ),
+                          Text(" You should enter numbers!!!"),],
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text("OK"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }
             },
             child: Text("Calculate"),
           )
