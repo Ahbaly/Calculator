15311dcc50e6c0aba84d81d37861980a250e95c6
Ahbaly <123125056+Ahbaly@users.noreply.github.com>
2023-07-09 16:11:03 +0100

    get info data from Json

--- lib/screens/Info.dart
+++ lib/screens/Info.dart
@@ -1,21 +1,43 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Info extends StatefulWidget {
   const Info({super.key});
 
   @override
   State<Info> createState() => _InfoState();
+
 }
 
 class _InfoState extends State<Info> {
-  final List<String> data = <String>[
-    'About: Perform some basic arithmetic operations',
-    'Year: 2023',
-    'Developer: Mohamed AHBALY',
-    'Email: ahbalymo@gmail.com',
-    'Phone: 07 17 XX XX XX'
-  ];
+  var jsonData;
+  var Keys = ["About", "Year", "Developer","Email","Phone"];
+
+  // final List<String> data = <String>[
+  //   'About: Perform some basic arithmetic operations',
+  //   'Year: 2023',
+  //   'Developer: Mohamed AHBALY',
+  //   'Email: ahbalymo@gmail.com',
+  //   'Phone: 07 17 XX XX XX'
+  // ];
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/info.json');
+    final data = await json.decode(response);
+    setState(() {
+      jsonData = data;
+      print(jsonData);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readJson();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +51,7 @@ class _InfoState extends State<Info> {
       ),
       child: ListView.builder(
           padding: const EdgeInsets.all(8),
-          itemCount: data.length,
+          itemCount: Keys.length,
           itemBuilder: (BuildContext context, int index) {
             return Container(
               margin: EdgeInsets.all(5.0),
@@ -42,13 +64,15 @@ class _InfoState extends State<Info> {
                 ),
               ),
               child: Text(
-                '${data[index]}',
+                '${Keys[index]} : ${jsonData[Keys[index]]}',
                 style: TextStyle(
                   backgroundColor: Colors.amber.shade100,
                   fontSize: 17,
                 ),
+
               ),
             );
+
           }),
     );
   }
